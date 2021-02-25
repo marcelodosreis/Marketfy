b2735dd9e71b4583c6d92e3b2dea11305b67505d
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-02-25 20:22:01 -0300

    feat: creating base home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 import Router from 'next/router';
 import App from 'next/app';
-import Link from 'next/link';
+
+import Header from '../library/common/components/Header/Header';
+import Cart from '../library/common/components/Cart/Cart';
 
 import 'tailwindcss/tailwind.css';
 
@@ -19,20 +21,8 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <nav>
-                <style jsx>{`
-                    a {
-                        margin: 0 10px 0 0;
-                    }
-                `}</style>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-                <a href="/non-existing">Non Existing Page</a>
-            </nav>
+            <Header />
+            <Cart />
             <Component {...pageProps} />
         </>
     );
