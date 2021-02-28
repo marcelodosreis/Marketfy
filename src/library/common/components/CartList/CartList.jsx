e32a4da80aa5a707d75d7c26df98a31045312bfa
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-02-28 15:39:53 -0300

    feat: removing svg tags

--- src/library/common/components/CartList/CartList.jsx
+++ src/library/common/components/CartList/CartList.jsx
@@ -8,8 +8,10 @@ import {
 
 import Button from '../Button/Button';
 import CartItem from './CartItem/CartItem';
+import PromoteCode from './PromoteCode/PromoteCode';
 
 import { BsArrowRight } from 'react-icons/bs';
+import { GrClose } from 'react-icons/gr';
 
 const CartList = () => {
     const { isOpen, cartList, hasNewProducts } = useSelector((state) => state.cart);
@@ -29,31 +31,15 @@ const CartList = () => {
                 <button
                     onClick={() => dispatch(toggleCartSidebar(!isOpen))}
                     className="text-gray-600 focus:outline-none">
-                    <svg
-                        className="h-5 w-5"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
+                    <GrClose className="h-5 w-5" />
                 </button>
             </div>
             <hr className="my-3" />
             {cartList?.map(({ id, name, imageUrl, price }) => (
                 <CartItem key={id} name={name} imageUrl={imageUrl} price={price} />
             ))}
-            <div className="mt-8">
-                <form className="flex items-center justify-center">
-                    <input className="form-input w-48" type="text" placeholder="Add promocode" />
-                    <button className="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                        <span>Apply</span>
-                    </button>
-                </form>
-            </div>
-            <Button icon={<BsArrowRight />}>Checkout</Button>
+            <PromoteCode />
+            <Button icon={<BsArrowRight className="h-5 w-5" />}>Checkout</Button>
         </div>
     );
 };
--- src/library/common/components/Header/Header.jsx
+++ src/library/common/components/Header/Header.jsx
@@ -5,6 +5,10 @@ import Link from 'next/link';
 
 import { toggleCartSidebar } from '../../../../main/store/modules/cart/action';
 
+import { GrLocation, GrFormSearch } from 'react-icons/gr';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 const Header = () => {
     const { isOpen: cartIsOpen, hasNewProducts } = useSelector((state) => state.cart);
     const [headerIsOpen, setHeaderIsOpen] = useState(false);
@@ -15,24 +19,7 @@ const Header = () => {
             <div className="container mx-auto px-6 py-3">
                 <div className="flex items-center justify-between">
                     <div className="hidden w-full text-gray-600 md:flex md:items-center">
-                        <svg
-                            className="h-5 w-5"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.06298 10.063 6.27212 12.2721 6.27212C14.4813 6.27212 16.2721 8.06298 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16755 11.1676 8.27212 12.2721 8.27212C13.3767 8.27212 14.2721 9.16755 14.2721 10.2721Z"
-                                fill="currentColor"
-                            />
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.39409 5.48178 3.79417C8.90918 0.194243 14.6059 0.054383 18.2059 3.48178C21.8058 6.90918 21.9457 12.6059 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.97318 6.93028 5.17324C9.59603 2.3733 14.0268 2.26452 16.8268 4.93028C19.6267 7.59603 19.7355 12.0268 17.0698 14.8268Z"
-                                fill="currentColor"
-                            />
-                        </svg>
+                        <GrLocation className="h-5 w-5" />
                         <span className="mx-1 text-sm">NY</span>
                     </div>
                     <div className="w-full text-gray-700 md:text-center text-2xl font-semibold">
@@ -43,19 +30,7 @@ const Header = () => {
                             <button
                                 onClick={() => dispatch(toggleCartSidebar(!cartIsOpen))}
                                 className="relative text-gray-700 hover:text-gray-600">
-                                <svg
-                                    className="h-5 w-5"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                </svg>
+                                <HiOutlineShoppingCart className="h-5 w-5" />
 
                                 {hasNewProducts && (
                                     <span className="absolute top-0 left-0 rounded-full bg-indigo-500 text-white p-1 text-xs"></span>
@@ -69,11 +44,7 @@ const Header = () => {
                                 type="button"
                                 className="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500"
                                 aria-label="toggle menu">
-                                <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                                </svg>
+                                <GiHamburgerMenu className="h-6 w-6 fill-current" />
                             </button>
                         </div>
                     </div>
@@ -112,15 +83,7 @@ const Header = () => {
                 </nav>
                 <div className="relative mt-6 max-w-lg mx-auto">
                     <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                        <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
-                            <path
-                                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
+                        <GrFormSearch className="h-5 w-5 text-gray-500" />
                     </span>
 
                     <input
