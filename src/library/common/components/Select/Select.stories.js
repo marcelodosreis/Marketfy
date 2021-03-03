3bf4644679867f1ce1a69eecc582fcdc98e8ce1a
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-03-03 17:00:20 -0300

    chore: remove svg imports in storybook

--- src/library/common/components/Select/Select.stories.js
+++ src/library/common/components/Select/Select.stories.js
@@ -2,14 +2,10 @@ import React, { useState } from 'react';
 
 import Select from './Select';
 
-import brasilLogo from '../../../../../public/images/svg/brIcon.svg';
-import usaLogo from '../../../../../public/images/svg/enIcon.svg';
-import spanishLogo from '../../../../../public/images/svg/esIcon.svg';
-
 const selectMock = [
-    { label: 'Português', value: 'pt-BR', icon: brasilLogo },
-    { label: 'English', value: 'en-US', icon: usaLogo },
-    { label: 'Español', value: 'es-ES', icon: spanishLogo }
+    { label: 'Português', value: 'pt-BR' },
+    { label: 'English', value: 'en-US' },
+    { label: 'Español', value: 'es-ES' }
 ];
 
 const Story = () => {
