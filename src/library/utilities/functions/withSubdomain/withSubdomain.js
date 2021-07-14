6c9b9ef1d173438ff52f9436f55d5137483782bd
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-07-14 15:22:36 -0300

    refactor: subdomain

--- src/library/utilities/functions/withSubdomain/withSubdomain.js
+++ src/library/utilities/functions/withSubdomain/withSubdomain.js
@@ -1,14 +1,12 @@
-export function withSubdomain(getServerSideProps, domain) {
+export function withSubdomain(getServerSideProps, domainUrl, pagePath) {
     return async (context) => {
         const urlHost = context.req?.headers?.host;
-        const loginUrl = domain;
-        const pageUrl = context.req.url.includes('?') && context.req.url.split('?')[0];
 
-        if (context.res && urlHost !== loginUrl) {
-            context.res.writeHead(301, {
-                Location: `http://${loginUrl}/${pageUrl}`
+        if (context.res && urlHost !== domainUrl) {
+            await context.res.writeHead(301, {
+                Location: `http://${domainUrl}/${pagePath}`
             });
-            context.res.end();
+            await context.res.end();
         }
         return await getServerSideProps(context);
     };
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,10 +3,14 @@ import React from 'react';
 
 const LoginPage = () => <p>Login (Next.js!)</p>;
 
-export const getServerSideProps = withSubdomain(() => {
-    return {
-        props: {}
-    };
-}, 'account.commercify.app:8080');
+export const getServerSideProps = withSubdomain(
+    () => {
+        return {
+            props: {}
+        };
+    },
+    'account.commercify.app:8080',
+    'login'
+);
 
 export default LoginPage;
