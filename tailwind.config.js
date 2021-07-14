7b565d013fbf5d530d919d9f740f21b5aaa37631
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-07-14 16:04:36 -0300

    fix: add correct tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    purge: ['.src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
+    mode: 'jit',
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false,
     theme: {
         extend: {}
     },
